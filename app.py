db8d10fa8224e5cd0b43b3fd2e269fa9bb205d83
CASPAR415 <18982293068@163.com>
2025-07-11 18:31:44 +0800

    Make app.py compatible with Render PORT env

--- app.py
+++ app.py
@@ -95,4 +95,5 @@ def prices():
         return jsonify({"error": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
